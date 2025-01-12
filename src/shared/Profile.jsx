ea091b880dc410456dd04d86545c2fc9de794771
Nafiz Al Turabi <nafizalturabi18cse@gmail.com>
2025-01-12 17:16:54 +0600

    profile added

--- src/shared/Profile.jsx
+++ src/shared/Profile.jsx
@@ -4,17 +4,139 @@ const Profile = () => {
     return (
         <div className='bg-white p-8'>
             <form action="">
-                <div class="relative h-11 w-full min-w-[200px]">
-                    <input
-                        placeholder=""
-                        class="peer h-full w-full rounded-md border border-blue-gray-200 border-t-transparent bg-transparent px-3 py-3 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-cyan-500 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
-                    />
-                    <label
-                        class="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight text-blue-gray-400 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[4.1] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-cyan-500 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:!border-cyan-500 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:!border-cyan-500 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500"
-                    >
-                        Email
-                    </label>
+                {/* this will add image */}
+                <div className='w-20 h-20 bg-slate-400 rounded-full mb-12'>
+                    <img src="" alt="" />
                 </div>
+                <div className='grid grid-cols-1  lg:grid-cols-12 gap-6'>
+                    <div className="relative col-span-6">
+                        <input
+                            type="text"
+                            className="peer w-full px-3 py-4 text-[#64636A] text-base font-bold border rounded-lg placeholder-transparent focus:outline-none focus:border-blue-500"
+                        />
+                        <label
+                            htmlFor=''
+                            className="absolute left-3 -top-2.5 bg-white px-1 text-sm text-gray-600 transition-all peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400 peer-placeholder-shown:top-3.5 peer-focus:-top-2.5 peer-focus:text-sm peer-focus:text-blue-500"
+                        >
+                            Full Name*
+                        </label>
+
+                    </div>
+                    <div className="relative col-span-6">
+                        <input
+                            type="text"
+                            className="peer w-full px-3 py-4 text-[#64636A] text-base font-bold border rounded-lg placeholder-transparent focus:outline-none focus:border-blue-500"
+                        />
+                        <label
+                            htmlFor=''
+                            className="absolute left-3 -top-2.5 bg-white px-1 text-sm text-gray-600 transition-all peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400 peer-placeholder-shown:top-3.5 peer-focus:-top-2.5 peer-focus:text-sm peer-focus:text-blue-500"
+                        >
+                            Email*
+                        </label>
+
+                    </div>
+                    <div className="relative col-span-6">
+                        <input
+                            type="text"
+                            className="peer w-full px-3 py-4 text-[#64636A] text-base font-bold border rounded-lg placeholder-transparent focus:outline-none focus:border-blue-500"
+                        />
+                        <label
+                            htmlFor=''
+                            className="absolute left-3 -top-2.5 bg-white px-1 text-sm text-gray-600 transition-all peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400 peer-placeholder-shown:top-3.5 peer-focus:-top-2.5 peer-focus:text-sm peer-focus:text-blue-500"
+                        >
+                            Phone No*
+                        </label>
+                    </div>
+                    <div className="relative col-span-6">
+                        <input
+                            type="date"
+                            className="peer w-full px-3 py-4 text-[#64636A] text-base font-bold border rounded-lg placeholder-transparent focus:outline-none focus:border-blue-500"
+                        />
+                        <label
+                            htmlFor=''
+                            className="absolute left-3 -top-2.5 bg-white px-1 text-sm text-gray-600 transition-all peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400 peer-placeholder-shown:top-3.5 peer-focus:-top-2.5 peer-focus:text-sm peer-focus:text-blue-500"
+                        >
+                            Date Of Birth*
+                        </label>
+                    </div>
+                    <div className="relative col-span-6 lg:col-span-4">
+                        <input
+                            type="text"
+                            className="peer w-full px-3 py-4 text-[#64636A] text-base font-bold border rounded-lg placeholder-transparent focus:outline-none focus:border-blue-500"
+                        />
+                        <label
+                            htmlFor=''
+                            className="absolute left-3 -top-2.5 bg-white px-1 text-sm text-gray-600 transition-all peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400 peer-placeholder-shown:top-3.5 peer-focus:-top-2.5 peer-focus:text-sm peer-focus:text-blue-500"
+                        >
+                            NID / Passport
+                        </label>
+                    </div>
+                    
+                    <div className="relative col-span-6 lg:col-span-4">
+                        <input
+                            type="text"
+                            className="peer w-full px-3 py-4 text-[#64636A] text-base font-bold border rounded-lg placeholder-transparent focus:outline-none focus:border-blue-500"
+                        />
+                        <label
+                            htmlFor=''
+                            className="absolute left-3 -top-2.5 bg-white px-1 text-sm text-gray-600 transition-all peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400 peer-placeholder-shown:top-3.5 peer-focus:-top-2.5 peer-focus:text-sm peer-focus:text-blue-500"
+                        >
+                            Country*
+                        </label>
+                    </div>
+                    <div className="relative col-span-6 lg:col-span-4">
+                        <input
+                            type="text"
+                            className="peer w-full px-3 py-4 text-[#64636A] text-base font-bold border rounded-lg placeholder-transparent focus:outline-none focus:border-blue-500"
+                        />
+                        <label
+                            htmlFor=''
+                            className="absolute left-3 -top-2.5 bg-white px-1 text-sm text-gray-600 transition-all peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400 peer-placeholder-shown:top-3.5 peer-focus:-top-2.5 peer-focus:text-sm peer-focus:text-blue-500"
+                        >
+                            State*
+                        </label>
+                    </div>
+                    <div className="relative col-span-6 lg:col-span-4">
+                        <input
+                            type="text"
+                            className="peer w-full px-3 py-4 text-[#64636A] text-base font-bold border rounded-lg placeholder-transparent focus:outline-none focus:border-blue-500"
+                        />
+                        <label
+                            htmlFor=''
+                            className="absolute left-3 -top-2.5 bg-white px-1 text-sm text-gray-600 transition-all peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400 peer-placeholder-shown:top-3.5 peer-focus:-top-2.5 peer-focus:text-sm peer-focus:text-blue-500"
+                        >
+                            City*
+                        </label>
+                    </div>
+                    <div className="relative col-span-6 lg:col-span-4">
+                        <input
+                            type="text"
+                            className="peer w-full px-3 py-4 text-[#64636A] text-base font-bold border rounded-lg placeholder-transparent focus:outline-none focus:border-blue-500"
+                        />
+                        <label
+                            htmlFor=''
+                            className="absolute left-3 -top-2.5 bg-white px-1 text-sm text-gray-600 transition-all peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400 peer-placeholder-shown:top-3.5 peer-focus:-top-2.5 peer-focus:text-sm peer-focus:text-blue-500"
+                        >
+                            Address*
+                        </label>
+                    </div>
+                    <div className="relative col-span-6 lg:col-span-4">
+                        <input
+                            type="text"
+                            className="peer w-full px-3 py-4 text-[#64636A] text-base font-bold border rounded-lg placeholder-transparent focus:outline-none focus:border-blue-500"
+                        />
+                        <label
+                            htmlFor=''
+                            className="absolute left-3 -top-2.5 bg-white px-1 text-sm text-gray-600 transition-all peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400 peer-placeholder-shown:top-3.5 peer-focus:-top-2.5 peer-focus:text-sm peer-focus:text-blue-500"
+                        >
+                            Zip Code*
+                        </label>
+                    </div>
+                    
+                </div>
+                <button type='submit' className='bg-gradient-to-l to-[#4A90E2] from-[#1565C0] text-white py-5 px-6 rounded-md mt-8 hover:scale-105 duration-300 '>
+                    Save Changes
+                </button>
             </form>
         </div>
     );
