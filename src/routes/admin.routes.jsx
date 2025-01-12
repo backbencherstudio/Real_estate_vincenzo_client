86293883b318d44e9533f051e5f0c8b8bf9155a5
sr sohan <sr.sohan088@gmail.com>
2025-01-12 10:38:36 +0600

    add route

--- src/routes/admin.routes.jsx
+++ src/routes/admin.routes.jsx
@@ -5,40 +5,38 @@ import AdminProfile from "../pages/Admin/Settings/AdminProfile";
 import TenantDetails from "../pages/Admin/TenantDetails/TenantDetails";
 
 export const adminPaths = [
-    {
-        name: "Dashboard",
-        path: "dashboard",
-        element: <AdminDashboard />,
-    },
-    {
-        name: "User Managment",
-        children: [
-            {
-                name: "Create Owner",
-                path: "create-owner",
-                element: <CreateOwner />,
-            },
-            {
-                name: "Tenant",
-                path: "tenant",
-                element: <AllTenant />,
-            },
-            {
-                path: "tenant/:id",
-                element: <TenantDetails />,
-            },
-
-        ],
-    },
-    {
-        name: "Settings",
-        children: [
-            {
-                name: "Profile",
-                path: "profile",
-                element: <AdminProfile />
-            }
-        ]
-    }
+  {
+    name: "Dashboard",
+    path: "dashboard",
+    element: <AdminDashboard />,
+  },
+  {
+    name: "User Managment",
+    children: [
+      {
+        name: "Create Owner",
+        path: "create-owner",
+        element: <CreateOwner />,
+      },
+      {
+        name: "Tenant",
+        path: "tenant",
+        element: <AllTenant />,
+      },
+      {
+        path: "tenant/:id",
+        element: <TenantDetails />,
+      },
+    ],
+  },
+  {
+    name: "Settings",
+    children: [
+      {
+        name: "Profile",
+        path: "profile",
+        element: <AdminProfile />,
+      },
+    ],
+  },
 ];
-
--- src/routes/owner.routes.jsx
+++ src/routes/owner.routes.jsx
@@ -1,6 +1,8 @@
 import OwnerDashboard from "../pages/Owner/Owner";
 import AddProperties from "../pages/Owner/Properties/AddProperties";
 import Properties from "../pages/Owner/Properties/Properties";
+import TenantDetails from "../pages/Owner/Properties/TenantDetails";
+import Tenants from "../pages/Owner/Properties/Tenants";
 
 export const ownerPaths = [
   {
@@ -15,6 +17,15 @@ export const ownerPaths = [
         name: "Properties",
         path: "properties",
         element: <Properties />,
+      },
+      {
+        name: "Tenants",
+        path: "tenants",
+        element: <Tenants />,
+      },
+      {
+        path: "tenants/:id",
+        element: <TenantDetails />,
       },
       {
         path: "properties/addProperties",
