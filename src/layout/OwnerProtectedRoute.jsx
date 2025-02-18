42c320ebe13b118b9b1a414316fdcd732676f10d
Fozle Rabbi <fozlerabbishuvo@gmail.com>
2025-02-18 17:20:12 +0600

    working on owner protected route

--- src/layout/OwnerProtectedRoute.jsx
+++ src/layout/OwnerProtectedRoute.jsx
@@ -1,52 +1,90 @@
 /* eslint-disable react/prop-types */
-import { useSelector, useDispatch } from "react-redux";
-import { Navigate } from "react-router-dom";
-import { logOut, useCurrentToken } from "../redux/fetures/auth/authSlice";
-import { verifyToken } from "../utils/varifyToken";
-import ownerApi from "../redux/fetures/owner/ownerApi";
+ import { useSelector, useDispatch } from "react-redux";
+ import { Navigate } from "react-router-dom";
+ import { logOut, useCurrentToken } from "../redux/fetures/auth/authSlice";
+ import { verifyToken } from "../utils/varifyToken";
+ import ownerApi from "../redux/fetures/owner/ownerApi";
+ const OwnerProtectedRoute = ({ children, role }) => {
+   const token = useSelector(useCurrentToken);
+   const dispatch = useDispatch();
+   const user = token ? verifyToken(token) : null;
+   const { data, isLoading } = ownerApi.useIsOwnerActiveQuery(user?.email, {
+     skip: !user?.email,
+   });
 
-const OwnerProtectedRoute = ({ children, role }) => {
-  const token = useSelector(useCurrentToken);
-  const dispatch = useDispatch();
-  let user;
+   console.log(data?.data);
+   
 
+   if (isLoading) {
+     return (
+       <div className="w-full h-screen flex items-center justify-center">
+         <p className="text-xl text-center font-semibold">Loading...</p>
+       </div>
+     );
+   }
+   
+   if (data?.data?.subscriptionStatus !== "active") {
+     dispatch(logOut());
+     return <Navigate to="/subscription-plan" replace />;
+   }
+   
+   if (role && role !== data?.data?.role) {
+     dispatch(logOut());
+     return <Navigate to="/signin" replace />;
+   }
 
-  if (token) {
-    user = verifyToken(token);
-  }
-
-  const { data, isLoading } = ownerApi.useIsOwnerActiveQuery(user?.email, {
-    skip: !user?.email,
-  });
-
-  if (isLoading) {
-    return <div className="w-ful h-screen flex items-center justify-center" >
-      <p className="text-xl text-center font-semibold" >Loading...</p>
-    </div>
-  }
-
-  if (user?.customerId && !user?.customerId) {
-    dispatch(logOut());
-    return <Navigate to="/signin" replace />;
-  }
-
-  // if (!user?.subscriptionStatus || user?.subscriptionStatus !== "active"  ) {
-  if (!user?.subscriptionStatus || data?.data?.subscriptionStatus !== "active") {
-    dispatch(logOut());
-    return <Navigate to="/subscription-plan" replace />;
-  }
-
-  if (role !== undefined && role !== user?.role) {
-    dispatch(logOut());
-    return <Navigate to="/signin" replace />;
-  }
-
-  if (!token) {
-    return <Navigate to="/signin" replace />;
-  }
+   return children;
+ };
+ export default OwnerProtectedRoute;
 
 
-  return children;
-};
 
-export default OwnerProtectedRoute;
+
+
+
+
+
+
+
+
+
+
+
+/* eslint-disable react/prop-types */
+// import { useSelector, useDispatch } from "react-redux";
+// import { Navigate } from "react-router-dom";
+// import { logOut, useCurrentToken } from "../redux/fetures/auth/authSlice";
+// import { verifyToken } from "../utils/varifyToken";
+
+// const OwnerProtectedRoute = ({ children, role }) => {
+//   const token = useSelector(useCurrentToken);
+//   const dispatch = useDispatch();
+
+//   let user;
+//   if (token) {
+//     user = verifyToken(token);
+//   }
+  
+
+//   if (user?.customerId && !user?.customerId) {
+//     dispatch(logOut());
+//     return <Navigate to="/signin" replace />;
+//   }
+
+//   if (!user?.subscriptionStatus || user?.subscriptionStatus !== "active") {
+//     return <Navigate to="/subscription-plan" replace />;
+//   }
+
+//   if (role !== undefined && role !== user?.role) {
+//     dispatch(logOut());
+//     return <Navigate to="/signin" replace />;
+//   }
+
+//   if (!token) {
+//     return <Navigate to="/signin" replace />;
+//   }
+
+//   return children;
+// };
+
+// export default OwnerProtectedRoute;
