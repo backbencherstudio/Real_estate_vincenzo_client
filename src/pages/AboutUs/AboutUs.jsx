150376ef95ac550c39090632e91a17394b0283b9
Nafiz-Al-Turabi <nafizalturabi18cse@gmail.com>
2025-02-22 14:53:54 +0600

    Header content added

--- src/pages/AboutUs/AboutUs.jsx
+++ src/pages/AboutUs/AboutUs.jsx
@@ -1,8 +1,15 @@
-
+import landing from "../../assets/landing.svg"
+import HeaderContent from "../../components/Header/Header";
 const AboutUs = () => {
+    const Contents = {
+        title: "Effective Tenant Management for Superior Property Performance",
+        description: "Effective tenant management boosts property performance and ensures steady income. Strong relationships and timely support lead to smoother operations.",
+        buttonText: "About Us",
+        images: [landing, landing],
+    };
     return (
         <div>
-            <h2>About Us</h2>
+            <HeaderContent content={Contents} />
         </div>
     );
 };
