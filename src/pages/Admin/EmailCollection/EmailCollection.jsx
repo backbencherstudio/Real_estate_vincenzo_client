3a8d6ee69532e88ae335cefb85b922be491a326e
Fozle Rabbi <fozlerabbishuvo@gmail.com>
2025-02-25 15:20:13 +0600

    call delete function

--- src/pages/Admin/EmailCollection/EmailCollection.jsx
+++ src/pages/Admin/EmailCollection/EmailCollection.jsx
@@ -1,13 +1,14 @@
 import adminApi from '../../../redux/fetures/admin/adminApi';
 
 const EmailCollection = () => {
-    const {data} = adminApi.useGetAllEmailCollectionDataQuery();
+    const { data, isLoading } = adminApi.useGetAllEmailCollectionDataQuery();
+    const [deleteEmailCollectionData, { isLoading: emailDeleteIsLoading }] = adminApi.useDeleteEmailCollectionDataMutation()
     console.log(data?.data);
 
-    
+
     return (
         <div>
-            
+
         </div>
     );
 };
