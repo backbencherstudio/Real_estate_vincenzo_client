aa76bbb64773f9f0dc9f54591a71a3592df13aa7
Nafiz-Al-Turabi <nafizalturabi18cse@gmail.com>
2025-02-26 16:01:59 +0600

    updated

--- src/pages/Admin/EmailCollection/EmailCollection.jsx
+++ src/pages/Admin/EmailCollection/EmailCollection.jsx
@@ -41,7 +41,7 @@ const EmailCollection = () => {
     return (
         <div className="">
             <h1 className="text-2xl font-bold mb-4">Email Collection</h1>
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3  xl:grid-cols-6 gap-4">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2  xl:grid-cols-4 2xl:grid-cols-5 gap-4">
                 {isLoading ? (
                     <p>Loading...</p>
                 ) : (
