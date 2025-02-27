49e21c6f492e7c89aa6a290a9896002b6476ac52
Nafiz-Al-Turabi <nafizalturabi18cse@gmail.com>
2025-02-27 10:43:22 +0600

    Updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 
+import { useEffect } from 'react'
 import './App.css'
 import MainLayout from './layout/MainLayout'
 
 function App() {
-
+  useEffect(()=>{
+    window.scrollTo(0,0)
+  },[])
   return (
     <div className='max-w-[1920px] mx-auto bg-green-600 ' >
      <MainLayout></MainLayout>
